c8d3b8769a0274214668401393c358a6c9521fda
KOKOMI12345 <3072252442@qq.com>
2025-03-24 19:49:07 +0800

    准备跨平台

--- PyMemDump/kernelCore.py
+++ PyMemDump/kernelCore.py
@@ -4,9 +4,12 @@ import sys
 
 if sys.platform == "win32":
     kernel32 = ctypes.WinDLL("kernel32", use_last_error=True)
+    """ Windows Kernel Core"""
 
 if sys.platform == "linux":
     LinuxKernelCore = ctypes.CDLL("libc.so.6")
+    """ Linux Kernel Core"""
 
 if sys.platform == "darwin":
     MacOsKernelCore = ctypes.CDLL("libSystem.B.dylib")
+    """ MacOs Kernel Core"""
