d706ad13f4eaf894ca93ba213049ea226074ce6c
KOKOMI12345 <3072252442@qq.com>
2025-03-26 18:04:59 +0800

    更新装饰器

--- PyMemDump/decorators.py
+++ PyMemDump/decorators.py
@@ -1,14 +1,18 @@
 """ my module decorators """
-from functools import wraps
 from typing import Callable, Any
+from functools import wraps
 import warnings
 
 class FutureFeature:
     """ Decorator for future implementation """
 
-    def __init__(self, version_implemented: str, ignore: bool = False) -> None:
+    def __init__(self, version_implemented: str, available_now: bool = False, is_a_idea: bool = False, ignore: bool = False) -> None:
         self.version_implemented = version_implemented
         """ the version of the function that is implemented """
+        self.available_now = available_now
+        """ whether the function is partially available now """
+        self.is_a_idea = is_a_idea
+        """ whether the function is just an idea """
         self.ignore = ignore
         """ ignore the warning message """
 
@@ -17,10 +21,14 @@ class FutureFeature:
         @wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
             warn_msg = f"""
-{func.__name__} is a future feature and is not implemented yet.
+{func.__name__} is a future feature and is not fully implemented yet.
 It will be implemented in version {self.version_implemented}.
-excepted functionality: {func.__doc__}
+Expected functionality: {func.__doc__}
 """
+            if not self.available_now:
+                warn_msg += "This function is not available yet.\n"
+            if self.is_a_idea:
+                warn_msg += "This function is just an idea and may not be implemented as described.\n"
             if not self.ignore:
                 warnings.warn(warn_msg, FutureWarning)
             return func(*args, **kwargs)
@@ -28,11 +36,11 @@ excepted functionality: {func.__doc__}
 
     def __repr__(self) -> str:
         """ return the representation of the decorator """
-        return f"<Future: {self.version_implemented}>"
+        return f"<Future: {self.version_implemented}, available_now={self.available_now}, is_a_idea={self.is_a_idea}>"
 
     def __str__(self) -> str:
         """ return the string representation of the decorator """
-        return f"Future: {self.version_implemented}"
+        return f"Future: {self.version_implemented}, available_now={self.available_now}, is_a_idea={self.is_a_idea}"
 
 # 示例用法
 if __name__ == "__main__":
